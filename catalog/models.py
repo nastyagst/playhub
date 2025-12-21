8f40fd9ecb1715d2722ff70b01411f8ce632445e
nastya <hociy2015@gmail.com>
2025-12-21 15:47:13 +0200

    Setup project: settings, auth user model and DB structure

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+class Gamer(AbstractUser):
+    bio = models.TextField(blank=True, verbose_name="About me")
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "gamer"
+        verbose_name_plural = "gamers"
+
+    def __str__(self):
+        return self.username
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Developer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    image = models.ImageField(upload_to="games/", blank=True, null=True)
+
+    developer = models.ForeignKey(Developer, on_delete=models.CASCADE, related_name="games")
+    genres = models.ManyToManyField(Genre, related_name="games")
+    gamers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="library", blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
--- playhub/settings.py
+++ playhub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'playhub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,4 +115,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
+AUTH_USER_MODEL = "catalog.Gamer"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+import os
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
