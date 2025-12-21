63f41da365ae60de456f4d4d00b7337679482dce
nastya <hociy2015@gmail.com>
2025-12-21 16:08:33 +0200

    Configure admin panel and register models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Gamer, Genre, Developer, Game, Comment
 
-# Register your models here.
+
+@admin.register(Gamer)
+class GamerAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional information", {"fields": ("bio", "avatar")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional information", {"fields": ("bio", "avatar")}),
+    )
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("title", "developer", "release_date")
+    search_fields = ("title",)
+    list_filter = ("genres", "developer")
+
+
+admin.site.register(Genre)
+admin.site.register(Developer)
+admin.site.register(Comment)
