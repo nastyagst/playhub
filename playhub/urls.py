6ba6812674c1878268bc2ee47101cdce9aa4d1b0
nastya <hociy2015@gmail.com>
2026-01-13 20:25:01 +0200

    Enable media serving

--- playhub/urls.py
+++ playhub/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +14,5 @@ urlpatterns = [
     ),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("catalog.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
